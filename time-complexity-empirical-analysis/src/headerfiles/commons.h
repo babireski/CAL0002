69da7129cabe866a59cea94cb767ea520bedbdd7
babireski <elian.babireski@gmail.com>
2023-03-29 09:31:08 -0300

    Some progress

--- time-complexity-empirical-analysis/src/headerfiles/commons.h
+++ time-complexity-empirical-analysis/src/headerfiles/commons.h
@@ -1,5 +1,7 @@
 #ifndef _COMMONS_H_
 	#define _COMMONS_H_
+	#include <stdio.h>
+	#include <time.h>
 	void arraygenerator(int* array, int size, int threshold);
 	void arrayprinter(int* array, int size);
 	void swap(int* a, int* b);
--- time-complexity-empirical-analysis/src/main.c
+++ time-complexity-empirical-analysis/src/main.c
@@ -2,20 +2,50 @@
 #include "headerfiles/searches.h"
 #include "headerfiles/sorts.h"
 
-#define SIZE 1000
+#define FUNCTIONS 7
 #define ITERATIONS 10
+#define SIZE 1000
+#define THRESHOLD 10000
 
 int main()
 {
-	int array[SIZE] = {};
+	FILE* file = fopen("data/data.csv", "w");
 
-	for(int i = 1; i < SIZE + 1; i++)
+	if(file == NULL)
+		return 1;
+
+	fputs("Size,Bubblesort,Insertionsort,Quicksort,Mergesort,Heapsort,Linearsearch,Binarysearch\n", file);
+
+	int array[SIZE];
+	float means[FUNCTIONS];
+
+	for(int i = 10; i < SIZE + 1; i += 10)
 	{
+		fprintf(file, "%i,", i);
+
+		for(int j = 0; j < FUNCTIONS; j++)
+			means[j] = 0;
+
 		for(int j = 0; j < ITERATIONS; j++)
 		{
+			arraygenerator(array, i, THRESHOLD);
+			clock_t clocks;
 
+			/* Bubblesort */
+			clocks = clock();
+			bubblesort(array, i);
+			clocks = clocks - clock();
+
+
+			/* Insertionsort */
+			/* Quicksort */
+			/* Mergesort */
+			/* Heapsort */
+			/* Linearsearch */
+			/* Binarysearch */
 		}
 	}
 
+	fclose(file);
 	return 0;
 }
